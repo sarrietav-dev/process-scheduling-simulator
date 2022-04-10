705b9b878cac6c6925423eda4f6cab59cc823079
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-04-09 22:58:41 -0500

    Implement the run method

--- src/models/strategies/SJFStrategy.ts
+++ src/models/strategies/SJFStrategy.ts
@@ -1,15 +1,51 @@
-import type { SchedulingStrategy } from '../SchedulingStrategy';
+import type { ProcessStatistic, SchedulingStrategy } from '../SchedulingStrategy';
 import type Process from '../Process';
+import lodash from 'lodash';
 
 class SJFStrategy implements SchedulingStrategy {
 	constructor(processes: Process[]) {
 		this.processes = processes;
 	}
 
-	processes: Process[];
+	readonly processes: Process[];
+	private finishedProcesses: Process[] = [];
+	private statsByProcess: ProcessStatistic[] = [];
 
-	run(): { process: Process; startTime: number; endTime: number; waitTime: number }[] {
-		return [];
+	/**
+	 * Returns the processes that have been initialized in a certain timeframe.
+	 * @param currentTime timeframe upperbound
+	 * @private
+	 */
+	private spawnProcesses(currentTime: number): Process[] {
+		return this.processes.filter((process) => {
+			return process.arrivalTime <= currentTime;
+		});
+	}
+
+	private get lastEndTime() {
+		return this.statsByProcess.length === 0
+			? lodash.minBy(this.processes, 'arrivalTime').arrivalTime
+			: this.statsByProcess.at(-1).endTime;
+	}
+
+	run(): ProcessStatistic[] {
+		this.processes.forEach(() => {
+			const startTime = this.lastEndTime;
+
+			const unattendedProcesses = this.spawnProcesses(startTime).filter(
+				(process) => !this.finishedProcesses.includes(process)
+			);
+
+			const shortestCpuTimeProcess = lodash.minBy(unattendedProcesses, 'cpuTime');
+
+			const endTime = startTime + shortestCpuTimeProcess.cpuTime;
+			const waitTime = startTime - shortestCpuTimeProcess.arrivalTime;
+
+			this.statsByProcess.push({ process: shortestCpuTimeProcess, startTime, endTime, waitTime });
+			this.finishedProcesses.push(shortestCpuTimeProcess);
+		});
+
+		return this.statsByProcess;
 	}
 }
 
