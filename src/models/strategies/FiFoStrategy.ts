257605c03cd021d361a9873b305ac08735d0ce00
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-04-09 23:12:09 -0500

    Refactor method to remove dependency on an ordered array.

--- src/models/strategies/FiFoStrategy.ts
+++ src/models/strategies/FiFoStrategy.ts
@@ -1,5 +1,6 @@
 import type { ProcessStatistic, SchedulingStrategy } from '../SchedulingStrategy';
 import type Process from '../Process';
+import lodash from 'lodash';
 
 class FiFoStrategy implements SchedulingStrategy {
 	constructor(processes: Process[]) {
@@ -7,20 +8,33 @@ class FiFoStrategy implements SchedulingStrategy {
 	}
 
 	readonly processes: Process[];
+	private statsByProcess: ProcessStatistic[] = [];
+	private attendedProcesses: Process[] = [];
+
+	private get lastEndTime() {
+		return this.statsByProcess.length === 0
+			? lodash.minBy(this.processes, 'arrivalTime').arrivalTime
+			: this.statsByProcess.at(-1).endTime;
+	}
+
+	private get unattendedProcesses() {
+		return this.processes.filter((process) => !this.attendedProcesses.includes(process));
+	}
 
 	run(): ProcessStatistic[] {
-		let waitTimes: { process: Process; startTime: number; endTime: number; waitTime: number }[] =
-			[];
+		this.processes.forEach(() => {
+			const startTime = this.lastEndTime;
 
-		this.processes.forEach((process) => {
-			const startTime = waitTimes.length === 0 ? process.arrivalTime : waitTimes.at(-1).endTime;
+			const process = lodash.minBy(this.unattendedProcesses, 'arrivalTime');
+
 			const endTime = startTime + process.cpuTime;
 			const waitTime = startTime - process.arrivalTime;
 
-			waitTimes = [...waitTimes, { process, startTime, endTime, waitTime }];
+			this.statsByProcess.push({ process, startTime, endTime, waitTime });
+			this.attendedProcesses.push(process);
 		});
 
-		return waitTimes;
+		return this.statsByProcess;
 	}
 }
 
