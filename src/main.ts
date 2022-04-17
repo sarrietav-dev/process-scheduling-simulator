6ddba92ad640ad2f30c422f7dcfcb65dd0374bd8
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-04-16 23:38:59 -0500

    Import primeicons file

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from "./App.vue";
 import PrimeVue from "primevue/config";
 import "primevue/resources/themes/lara-light-indigo/theme.css";
 import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
 
 const app = createApp(App);
 
