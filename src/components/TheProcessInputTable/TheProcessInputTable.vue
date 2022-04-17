1faf47b6fc627b0fd21e34aa9803b1abf2adc9af
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-04-16 23:39:22 -0500

    Use prime datatable

--- src/components/TheProcessInputTable/TheProcessInputTable.vue
+++ src/components/TheProcessInputTable/TheProcessInputTable.vue
@@ -1,39 +1,69 @@
 <template>
-  <div class="gap-2.5 rounded-2xl bg-white p-10 shadow">
-    <TheTableHeader :with-priority="withPriority" />
-    <TheTableBody>
-      <ProcessEntry
-        v-for="process in scheduler.processes"
-        :with-priority="withPriority"
-        v-bind:key="process.name"
-        v-model:name="process.name"
-        v-model:arrival-time.number="process.arrivalTime"
-        v-model:cpu-time.number="process.cpuTime"
-        v-model:priority.number="process.priority"
-      />
-    </TheTableBody>
-    <TheAddProcessButton @click="addProcess" />
-    <TheStrategyList
-      :strategies="strategies"
-      v-model:strategyChecked="strategyChecked"
-    />
-    <slot name="run-button"></slot>
+  <div class="p-fluid">
+    <div class="card" style="height: 50%">
+      <DataTable
+        :value="scheduler.processes"
+        edit-mode="row"
+        data-key="name"
+        v-model:editing-rows="editingRows"
+        scroll-height="flex"
+        responsive-layout="scroll"
+        @row-edit-save="handleRowEdit"
+      >
+        <Column header="Process Name" field="name" style="width: 20%">
+          <template #editor="{ field, data }">
+            <InputText v-model="data[field]" autofocus />
+          </template>
+        </Column>
+
+        <Column header="Arrival Time" field="arrivalTime" style="width: 20%">
+          <template #editor="{ field, data }">
+            <InputNumber v-model="data[field]" autofocus />
+          </template>
+        </Column>
+        <Column header="CPU time" field="cpuTime" style="width: 20%">
+          <template #editor="{ field, data }">
+            <InputNumber v-model="data[field]" autofocus />
+          </template>
+        </Column>
+        <Column
+          row-editor
+          style="width: 10%; min-width: 8rem"
+          bodyStyle="text-align:center"
+        />
+      </DataTable>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref, watch } from "vue";
 
-import TheAddProcessButton from "@/components/TheProcessInputTable/components/TheAddProcessButton.vue";
-import ProcessEntry from "@/components/TheProcessInputTable/components/ProcessEntry.vue";
-import TheTableBody from "@/components/TheProcessInputTable/components/TheTableBody.vue";
-import TheTableHeader from "@/components/TheProcessInputTable/components/TheTableHeader.vue";
 import Process from "@/models/Process";
 import { useScheduler } from "@/stores/scheduler";
 import { getStrategies } from "@/utils/get-strategies";
-import TheStrategyList from "@/components/TheProcessInputTable/components/TheStrategyList.vue";
+
+import DataTable, { type DataTableRowEditSaveEvent } from "primevue/datatable";
+import Column from "primevue/column";
+import InputText from "primevue/inputtext";
+import InputNumber from "primevue/inputnumber";
 
 const scheduler = useScheduler();
+
+const editingRows = ref([]);
+
+function handleRowEdit(event: DataTableRowEditSaveEvent) {
+  const { newData, index } = event;
+
+  scheduler.$patch((state) => {
+    state.processes[index] = new Process(
+      newData.name,
+      newData.cpuTime,
+      newData.arrivalTime,
+      newData.priority
+    );
+  });
+}
 
 function addProcess() {
   scheduler.$patch((state) => {
