a08e2dfcc83eb63e23409aa28fc5c86d9214c217
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-04-16 23:54:41 -0500

    Refactor column rendering

--- src/components/TheProcessInputTable/TheProcessInputTable.vue
+++ src/components/TheProcessInputTable/TheProcessInputTable.vue
@@ -10,20 +10,15 @@
         responsive-layout="scroll"
         @row-edit-save="handleRowEdit"
       >
-        <Column header="Process Name" field="name" style="width: 20%">
+        <Column
+          v-for="column in columns"
+          :field="column.field"
+          :header="column.header"
+          :key="column.header"
+          style="width: 20%"
+        >
           <template #editor="{ field, data }">
-            <InputText v-model="data[field]" autofocus />
-          </template>
-        </Column>
-
-        <Column header="Arrival Time" field="arrivalTime" style="width: 20%">
-          <template #editor="{ field, data }">
-            <InputNumber v-model="data[field]" autofocus />
-          </template>
-        </Column>
-        <Column header="CPU time" field="cpuTime" style="width: 20%">
-          <template #editor="{ field, data }">
-            <InputNumber v-model="data[field]" autofocus />
+            <component :is="column.component" v-model="data[field]" autofocus />
           </template>
         </Column>
         <Column
@@ -51,6 +46,24 @@ import InputNumber from "primevue/inputnumber";
 const scheduler = useScheduler();
 
 const editingRows = ref([]);
+
+const columns = ref([
+  {
+    header: "Process Name",
+    field: "name",
+    component: InputText,
+  },
+  {
+    header: "Arrival Time",
+    field: "arrivalTime",
+    component: InputNumber,
+  },
+  {
+    header: "CPU Time",
+    field: "cpuTime",
+    component: InputNumber,
+  },
+]);
 
 function handleRowEdit(event: DataTableRowEditSaveEvent) {
   const { newData, index } = event;
