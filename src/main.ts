f503dc8974c9f46d5794e7c274054469b1ea4c83
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-04-16 22:41:36 -0500

    Use prime vue in the app

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+import "primevue/resources/primevue.min.css";
 
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(PrimeVue);
+
+app.component("DataTable", DataTable);
+app.component("DataTableColumn", Column);
 
 app.mount("#app");
