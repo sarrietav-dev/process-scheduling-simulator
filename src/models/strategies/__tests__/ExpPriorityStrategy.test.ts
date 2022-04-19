403787e57482845b24b4c0fdf977dd6a512d5f02
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-04-19 10:18:03 -0500

    Create failing tests form ExpPriorityStrategy.ts

---
+++ src/models/strategies/__tests__/ExpPriorityStrategy.test.ts
@@ -0,0 +1,85 @@
+import Process from "../../Process";
+import PriorityStrategy from "../PriorityStrategy";
+import type { ProcessStatistic } from "../../SchedulingStrategy";
+import ExpPriorityStrategy from "../ExpPriorityStrategy";
+
+describe("The algorithm works correctly", () => {
+  const results: ProcessStatistic[] = [
+    {
+      process: new Process("P2", 3, 2, 1),
+      startTime: 2,
+      endTime: 5,
+      waitTime: 0,
+    },
+    {
+      process: new Process("P4", 4, 6, 1),
+      startTime: 6,
+      endTime: 10,
+      waitTime: 0,
+    },
+    {
+      process: new Process("P3", 2, 4, 2),
+      startTime: 10,
+      endTime: 12,
+      waitTime: 6,
+    },
+    {
+      process: new Process("P1", 4, 0, 3),
+      startTime: [0, 12],
+      endTime: [2, 14],
+      waitTime: 10,
+    },
+  ];
+
+  test("when ordered", () => {
+    const processes: Process[] = [
+      new Process("P1", 4, 0, 3),
+      new Process("P2", 3, 2, 1),
+      new Process("P3", 2, 4, 2),
+      new Process("P4", 4, 6, 1),
+    ];
+
+    const strategy = new ExpPriorityStrategy(processes);
+
+    expect(strategy.execute()).toEqual(results);
+  });
+
+  test("when unordered", () => {
+    const processes: Process[] = [
+      new Process("P3", 2, 4, 2),
+      new Process("P1", 4, 0, 3),
+      new Process("P4", 4, 6, 1),
+      new Process("P2", 3, 2, 1),
+    ];
+
+    const strategy = new ExpPriorityStrategy(processes);
+
+    expect(strategy.execute()).toEqual(results);
+  });
+});
+
+test("the average computes correctly", () => {
+  const processes: Process[] = [
+    new Process("P3", 2, 4, 2),
+    new Process("P1", 4, 0, 3),
+    new Process("P4", 4, 6, 1),
+    new Process("P2", 3, 2, 1),
+  ];
+
+  const strategy = new ExpPriorityStrategy(processes);
+
+  expect(strategy.waitTimeAverage).toBe(4);
+});
+
+test("throws an error when not defining a priority in a process", () => {
+  const processes: Process[] = [
+    new Process("A", 3, 0),
+    new Process("B", 3, 1),
+    new Process("C", 2, 2),
+    new Process("D", 4, 4),
+  ];
+
+  expect(() => new PriorityStrategy(processes)).toThrow(
+    "Every process must have a priority defined"
+  );
+});
