5d5df8939e6688389078cf5ea80e394edf63a640
sarrietav-dev <25210925+sarrietav-dev@users.noreply.github.com>
2022-04-18 20:26:14 -0500

    Delete unused components

--- src/components/TheProcessInputTable/components/TheAddProcessButton.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div
-    class="col-span-3 rounded-2xl border-2 border-dashed border-gray-600 bg-gray-300 py-2.5 text-center"
-  >
-    Add new process
-  </div>
-</template>
--- src/components/TheProcessInputTable/components/TheTableBody.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="col-span-3 max-h-60 gap-y-2.5 divide-y-2 overflow-auto p-1">
-    <slot />
-  </div>
-</template>
